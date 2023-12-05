77c1b64c81d66f8e60c53d138256a2935545caca
MarkoMTP <marko.mtp8@gmail.com>
2023-12-05 17:56:43 +0100

    now the game plays until the cells are all full

--- java.js
+++ java.js
@@ -1,3 +1,38 @@
+
+const createPlayer = () =>  {
+let hasPlayed = false;
+
+if(!hasPlayed ) {
+
+
+  hasPlayed = true;
+}
+
+
+
+
+  const playerName = prompt("Player Name");
+  
+  let playerLetter;
+  while (true) {
+    playerLetter = prompt("Choose 'X' or 'O'").toUpperCase();
+    if (playerLetter === 'X' || playerLetter === 'O') {
+      break;
+    } else {
+      alert("Invalid choice. Please choose 'X' or 'O'.");
+    }
+  }
+
+  return { playerName, playerLetter };
+}
+
+const { playerName, playerLetter } = createPlayer();
+
+
+
+
+
+
 function Gameboard() {
 
 const row = 3;
@@ -16,110 +51,109 @@ for(let j = 0; j < column; j++) {
 
 
 
-const displayBoard = (board) => {
-
-board.forEach(cell => {
+const printBoard = () => {
+  board.forEach(cell => {
     console.log(cell);
 
-
-})
-
-
-}
+  })
+};
 
 
 
+  
 
-function createPlayer() {
-    const playerName = prompt("Player Name");
-    
-    let playerLetter;
-    while (true) {
-      playerLetter = prompt("Choose 'X' or 'O'").toUpperCase();
-      if (playerLetter === 'X' || playerLetter === 'O') {
-        break;
-      } else {
-        alert("Invalid choice. Please choose 'X' or 'O'.");
+
+function isBoardFull() {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === "") {
+        // Found an empty cell, so the board is not full
+        return false;
       }
     }
-  
-    return { playerName, playerLetter };
   }
+  // All cells are filled, so the board is full
+  return true;
+}
 
-  const { playerName, playerLetter } = createPlayer();
 
-function playerMove() {
+
+
+
+
+
+
+
+
+return { printBoard, board, isBoardFull };
+};
+
+
+
+
+
+
+
+
+
+
+
+const gameControl = () => {
+  const { board, printBoard, isBoardFull } = Gameboard();
+
+
+  function playerMove() {
 
     
 
-    const row = prompt('Enter row number (0-2)');
-    const column = prompt('Enter column number (0-2)');
-   let playerChoice = '';
+  const row = prompt('Enter row number (0-2)');
+  const column = prompt('Enter column number (0-2)');
 
-
-       if(board[row][column] === "") {
-  return  playerChoice = board[row][column] = playerLetter
-       }  
-       else {
-        return alert('taken')
-       }
-
-
-
- 
+     if(board[row][column] === "") {
+  board[row][column] = playerLetter ,
+{row, column}
+     }  
+     else {
+       alert('Already played, Please select another cell')
+     }
 
 }
+
 
 
 function compChoice() {
-    const randomNum = Math.floor(Math.random() * 3);
-  
-    
-    function getRandompLetter() {
-      const pLetters = ['X', 'O'];
-      const randomIndex = Math.floor(Math.random() * pLetters.length);
-      return pLetters[randomIndex];
+  let emptyCellFound = false;
+  while (!emptyCellFound && !isBoardFull()) {
+    const randomRow = Math.floor(Math.random() * 3);
+    const randomColumn = Math.floor(Math.random() * 3);
+
+    if (board[randomRow][randomColumn] === '') {
+      board[randomRow][randomColumn] = playerLetter === 'X' ? 'O' : 'X';
+      emptyCellFound = true;
     }
-    
-    const randomLetter = getRandompLetter();
-    
-    // Assign the letter to the board without affecting the return value
-    board[randomNum][randomNum] = randomLetter;
-  
-    // Return the chosen letter
-    return randomLetter;
   }
-
-function playGame() {
-   
-   
-
-    playerMove()
- compChoice()
-   
-
-
-
-
-
 }
+while(!isBoardFull()) {
+
+playerMove()
+compChoice()
+printBoard()
+
+} 
+
+ 
 
 
 
-playGame() 
 
 
 
 
 
+};
 
 
-displayBoard(board)
-
-return { displayBoard }
+gameControl()
 
 
-
-}
-
-Gameboard()
+//jos neradi ova funckija za nastavit igrat dok nie puno i trebas play round dovrsit
